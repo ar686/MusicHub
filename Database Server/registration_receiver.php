1139fbf0ae9f2cc0f90d4d6506339b8808d5ce84
koliasoha <43789525+koliasoha@users.noreply.github.com>
2019-11-11 18:52:47 +0000

    Update registration_receiver.php
    error logging added

--- Database Server/registration_receiver.php
+++ Database Server/registration_receiver.php
@@ -1,4 +1,9 @@
 <?php
+ini_set("display_errors", 0);
+ini_set("log_errors",1);
+ini_set("error_log", "/tmp/error.log");
+error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT);
+
 require_once 'dbconfig.php';
 require_once 'path.inc';
 require_once 'rabbitMQLib.inc';
