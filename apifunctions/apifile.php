98efe9795b6ae1ffc3860f02a0ac477b1e98b61c
koliasoha <43789525+koliasoha@users.noreply.github.com>
2019-11-11 18:47:31 +0000

    Create apifile.php

---
+++ apifunctions/apifile.php
@@ -0,0 +1,25 @@
+?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+
+function search($name){
+  ini_set("allow_url_fopen", 1);
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://deezerdevs-deezer.p.rapidapi.com/search?q=$name",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+                "x-rapidapi-host: deezerdevs-deezer.p.rapidapi.com",
+                "x-rapidapi-key: 335c244f74msh59e1c3fea4460a2p1b1008jsna8fab81eacf0"
+        ),
+));
