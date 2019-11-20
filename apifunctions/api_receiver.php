4a30a738eaa3be96b8355293bdfed1eb33021882
koliasoha <43789525+koliasoha@users.noreply.github.com>
2019-11-20 13:14:24 -0500

    Update api_receiver.php

--- apifunctions/api_receiver.php
+++ apifunctions/api_receiver.php
@@ -33,7 +33,7 @@ curl_close($curl);
 if ($err) {
         echo "cURL Error #:" . $err;
 } else {
-        echo $response;
+        echo json_encode($response, JSON_PRETTY_PRINT);
 }
 
 function requestProcessor($request)
