89266be58a4ee367d506eee69db382c93b9b87e9
adr50 <56275859+adr50@users.noreply.github.com>
2019-11-20 22:47:43 -0500

    Create api_receiver.php

---
+++ API Server/api_receiver.php
@@ -0,0 +1,56 @@
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function requestProcessor($request){
+  echo "received request".PHP_EOL;
+  var_dump($request);
+    $results = doSearch($request['search']);
+    return $results;
+    }
+    //if($results == 1){
+        //return 0; // Server received request and processed it.
+    //}else{
+        //return 1;
+    //}
+
+function doSearch($search_string){
+  ini_set("allow_url_fopen", 1);
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://deezerdevs-deezer.p.rapidapi.com/search?q=$search_string",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+                "x-rapidapi-host: deezerdevs-deezer.p.rapidapi.com",
+                "x-rapidapi-key: 335c244f74msh59e1c3fea4460a2p1b1008jsna8fab81eacf0"
+        ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+        echo "cURL Error #:" . $err;
+        return 0;
+} else {
+        $r = json_encode($response);
+        //echo $r;
+        return $r;
+}
+}
+
+$server = new rabbitMQServer("api.ini","testServer");
+$server->process_requests('requestProcessor');
+exit();
+?>
