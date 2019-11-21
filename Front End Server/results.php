f118ed972f5171d22c660fc469eda0c56945b206
adr50 <56275859+adr50@users.noreply.github.com>
2019-11-20 22:36:01 -0500

    Create results.php

---
+++ Front End Server/results.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'path.inc';
+require_once 'rabbitMQLib.inc';
+require_once 'get_host_info.inc';
+
+$client = new rabbitMQClient("api.ini","testServer");
+
+$request = array(); // From POST, input username/password into the $request array.
+$request['search'] = $_GET['search'];
+$search = $request['search'];
+	
+$response = $client->send_request($request); // Store the response of send request.
+
+$array = json_decode($response, true);
+echo $array;
+
+?>
