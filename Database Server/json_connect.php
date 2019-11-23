99944bc29c4cf50b7cf2a05b9d4a0385aaf0fc2a
sbs97 <56939007+sbs97@users.noreply.github.com>
2019-11-22 23:47:07 -0500

    Add files via upload

---
+++ Database Server/json_connect.php
@@ -0,0 +1,21 @@
+<?php
+
+$connect = mysqli_connect("localhost", "root", "password", "website");
+
+$filename = ".json";
+
+$data = file_get_contents ($filename);
+
+$array = json_decode($data, true);
+
+foreach($array as $row)
+{
+	$sql = "INSERT INTO tbl_music(id, title, artist, duration, preview, picture) 
+	VALUES('".$row["id"]."','".$row["title"]."',
+	'".$row["name"]."','".$row["duration"]."',
+	'".$row["preview"]."','".$row["picture"]."',)";
+	
+	mysqli_query($connect, $sql);
+}
+
+?>
