ac9ba4223be70d59e70175a949e8083b10e35748
sbs97 <56939007+sbs97@users.noreply.github.com>
2019-11-25 13:38:32 -0500

    Update json_connect.php

--- Database Server/json_connect.php
+++ Database Server/json_connect.php
@@ -11,7 +11,8 @@ $array = json_decode($data, true);
 foreach($array as $row)
 {
 	$sql = "INSERT INTO tbl_music(id, title, artist, genre, duration, preview, picture) 
-	VALUES('".$row["id"]."','".$row["title"]."','".$row["name"]."','".$row["genre"]."','".$row["duration"]."','".$row["preview"]."','".$row["picture"]."',)";
+	VALUES('".$row["id"]."','".$row["title"]."','".$row["name"]."','".$row["genre"]."',
+	'".$row["duration"]."','".$row["preview"]."','".$row["picture"]."',)";
 	
 	mysqli_query($connect, $sql);
 }
