28f93b7b0493c07aebb5907c1534a37f409e6ac6
adr50 <56275859+adr50@users.noreply.github.com>
2019-11-25 01:28:08 -0500

    Update test_api_receiver.php

--- Test Files/test_api_receiver.php
+++ Test Files/test_api_receiver.php
@@ -4,7 +4,6 @@ ini_set("log_errors",1);
 ini_set("error_log", "/tmp/error.log");
 error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT);
 
-require_once 'dbconfig.php';
 require_once 'path.inc';
 require_once 'rabbitMQLib.inc';
 require_once 'get_host_info.inc';
