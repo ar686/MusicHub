83eafbaef7517d44f6cb074c2c39d51e2cc6aa30
koliasoha <43789525+koliasoha@users.noreply.github.com>
2019-11-24 23:06:35 -0500

    Create apigenre_receiver.php

---
+++ API Server/apigenre_receiver.php
@@ -0,0 +1,45 @@
+  
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+function requestProcessor($request){
+  echo "received request".PHP_EOL;
+  var_dump($request);
+    $results = SearchGenre($request['search']);
+    return $results;
+    }
+function SearchGenre($search_string){
+  ini_set("allow_url_fopen", 1);
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://deezerdevs-deezer.p.rapidapi.com/genre/%23country",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => array(
+		"x-rapidapi-host: deezerdevs-deezer.p.rapidapi.com",
+		"x-rapidapi-key: 335c244f74msh59e1c3fea4460a2p1b1008jsna8fab81eacf0"
+	),
+));
+  
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+if ($err) {
+        echo "cURL Error #:" . $err;
+        return 0;
+} else {
+        return $response;
+  }
+}
+$server = new rabbitMQServer("api.ini","testServer");
+$server->process_requests('requestProcessor');
+exit();
+?>
