a24e0c2462614507169d222506666083c2a17127
sbs97 <56939007+sbs97@users.noreply.github.com>
2019-11-25 13:38:02 -0500

    Update json_connect.php

--- Database Server/json_connect.php
+++ Database Server/json_connect.php
@@ -10,10 +10,8 @@ $array = json_decode($data, true);
 
 foreach($array as $row)
 {
-	$sql = "INSERT INTO tbl_music(id, title, artist, duration, preview, picture) 
-	VALUES('".$row["id"]."','".$row["title"]."',
-	'".$row["name"]."','".$row["duration"]."',
-	'".$row["preview"]."','".$row["picture"]."',)";
+	$sql = "INSERT INTO tbl_music(id, title, artist, genre, duration, preview, picture) 
+	VALUES('".$row["id"]."','".$row["title"]."','".$row["name"]."','".$row["genre"]."','".$row["duration"]."','".$row["preview"]."','".$row["picture"]."',)";
 	
 	mysqli_query($connect, $sql);
 }
